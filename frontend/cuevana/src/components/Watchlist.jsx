eb769e8b808f15d6a20c285a156114e833f752e8
Doom-2099 <3030878110108@ingenieria.usac.edu.gt>
2023-03-10 20:48:26 -0600

    [add] Template Para Watchlist
    Agregar template a utilizar para las watchlist, no contiene alguna logica o conexion con el backend aun

--- frontend/cuevana/src/components/Watchlist.jsx
+++ frontend/cuevana/src/components/Watchlist.jsx
@@ -1,13 +1,249 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 import "../styles/Watchlist.css";
 
-export function Watchlist() {
-  return (
-    <>
-      <div className="container-fluid px-5">
-        <div className="row gx-0">
-          <div className="col">
-            <ul className="pagination justify-content-center pagination-lg" style={{paddingTop: "2%"}}>
+export function Watchlist({ user }) {
+  const [listado, setListado] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getPeliculas();
+  }, []);
+
+  const getPeliculas = async () => {
+    const res = await axios.get("http://localhost:5000/verwatchlist"); // AGREGAR VAINAS PARA LA PETICION
+    if (res.data.res !== false) {
+      setListado(res.data.res);
+      setLoading(true);
+    } else {
+      toast.error("Error en la base", {
+        position: "top-right",
+        duration: 6000,
+        style: {
+          background: "bs-danger",
+          color: "#FFFF",
+        },
+      });
+    }
+  };
+
+  if (!loading) {
+    return (
+      <>
+        <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6"
+            href="#!"
+          >
+            Cuevana
+          </a>
+          <button
+            className="navbar-toggler position-absolute d-md-none collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu"
+            aria-controls="sidebarMenu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <input
+            className="form-control form-control-dark w-100 rounded-0 border-0"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <div className="navbar-nav">
+            <div className="nav-item text-nowrap">
+              <a className="nav-link px-3" href="#!">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-box-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+                  />
+                </svg>
+                &nbsp;Sign out
+              </a>
+            </div>
+          </div>
+        </header>
+
+        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-dark wi text-white">
+          <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 className="h2">Watchlist</h1>
+          </div>
+        </main>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6"
+            href="#!"
+          >
+            Cuevana
+          </a>
+          <button
+            className="navbar-toggler position-absolute d-md-none collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu"
+            aria-controls="sidebarMenu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <input
+            className="form-control form-control-dark w-100 rounded-0 border-0"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <div className="navbar-nav">
+            <div className="nav-item text-nowrap">
+              <a className="nav-link px-3" href="#!">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-box-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+                  />
+                </svg>
+                &nbsp;Sign out
+              </a>
+            </div>
+          </div>
+        </header>
+
+        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 wi text-white">
+          <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 className="h2">Watchlist</h1>
+          </div>
+          <div className="d-flex flex-wrap">
+            <div className="card card-size">
+              <img
+                src="https://static.cinepolis.com/img/peliculas/41658/1/1/41658.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-img-overlay bg-dark cuerpo">
+                <h5 className="card-text">Demon slayer</h5>
+                <p className="card-text" style={{ marginTop: "5%" }}>
+                  Score
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-star-fill"
+                    viewBox="0 0 16 16"
+                    style={{
+                      marginLeft: "5%",
+                    }}
+                  >
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-star-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-star-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-star-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-star-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </svg>
+                </p>
+                <div className="d-flex justify-content-center">
+                  <a className="btn btn-light" href="#!">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-bookmark-heart-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v13.5zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z" />
+                    </svg>
+                  </a>
+                  <a className="btn btn-info" href="#!">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-info-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <ul
+              className="pagination justify-content-center pagination-lg flex-wrap"
+              style={{ paddingTop: "2%" }}
+            >
               <li className="page-item">
                 <a href="#!" className="page-link">
                   <span>
@@ -64,154 +300,8 @@ export function Watchlist() {
               </li>
             </ul>
           </div>
-          <div className="w-100"></div>
-          <div className="col">
-            <div className="card p-3">
-              <img
-                src="https://i.pinimg.com/736x/f1/13/a0/f113a0ac7938bb5ff14bce7cc07ece60.jpg"
-                alt=""
-                className="card-img-top"
-                width={"200px"}
-                height={"300px"}
-              />
-              <div className="card-body">
-                <h5 className="card-title">Titulo Pelicula</h5>
-                <div className="row gy-2" id="rowButton">
-                  <button className="btn btn-primary">
-                    <i className="bi bi-arrows-angle-expand"></i>
-                    Ver Mas
-                  </button>
-                  <button className="btn btn-success">
-                    <i className="bi bi-stopwatch-fill"></i>
-                    Watchlist
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card p-3">
-              <img
-                src="https://i.pinimg.com/736x/f1/13/a0/f113a0ac7938bb5ff14bce7cc07ece60.jpg"
-                alt=""
-                className="card-img-top"
-                width={"200px"}
-                height={"300px"}
-              />
-              <div className="card-body">
-                <h5 className="card-title">Titulo Pelicula</h5>
-                <div className="row gy-2" id="rowButton">
-                  <a href="#!" className="btn btn-primary">
-                    <i className="bi bi-arrows-angle-expand"></i>
-                    Ver Mas
-                  </a>
-                  <a href="#!" className="btn btn-success">
-                    <i className="bi bi-stopwatch-fill"></i>
-                    Watchlist
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card p-3">
-              <img
-                src="https://i.pinimg.com/736x/f1/13/a0/f113a0ac7938bb5ff14bce7cc07ece60.jpg"
-                alt=""
-                className="card-img-top"
-                width={"200px"}
-                height={"300px"}
-              />
-              <div className="card-body">
-                <h5 className="card-title">Titulo Pelicula</h5>
-                <div className="row gy-2" id="rowButton">
-                  <a href="#!" className="btn btn-primary">
-                    <i className="bi bi-arrows-angle-expand"></i>
-                    Ver Mas
-                  </a>
-                  <a href="#!" className="btn btn-success">
-                    <i className="bi bi-stopwatch-fill"></i>
-                    Watchlist
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-100"></div>
-          <div className="col">
-            <div className="card p-3">
-              <img
-                src="https://i.pinimg.com/736x/f1/13/a0/f113a0ac7938bb5ff14bce7cc07ece60.jpg"
-                alt=""
-                className="card-img-top"
-                width={"200px"}
-                height={"300px"}
-              />
-              <div className="card-body">
-                <h5 className="card-title">Titulo Pelicula</h5>
-                <div className="row gy-2" id="rowButton">
-                  <a href="#!" className="btn btn-primary">
-                    <i className="bi bi-arrows-angle-expand"></i>
-                    Ver Mas
-                  </a>
-                  <a href="#!" className="btn btn-success">
-                    <i className="bi bi-stopwatch-fill"></i>
-                    Watchlist
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card p-3">
-              <img
-                src="https://i.pinimg.com/736x/f1/13/a0/f113a0ac7938bb5ff14bce7cc07ece60.jpg"
-                alt=""
-                className="card-img-top"
-                width={"200px"}
-                height={"300px"}
-              />
-              <div className="card-body">
-                <h5 className="card-title">Titulo Pelicula</h5>
-                <div className="row gy-2" id="rowButton">
-                  <a href="#!" className="btn btn-primary">
-                    <i className="bi bi-arrows-angle-expand"></i>
-                    Ver Mas
-                  </a>
-                  <a href="#!" className="btn btn-success">
-                    <i className="bi bi-stopwatch-fill"></i>
-                    Watchlist
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card p-3">
-              <img
-                src="https://i.pinimg.com/736x/f1/13/a0/f113a0ac7938bb5ff14bce7cc07ece60.jpg"
-                alt=""
-                className="card-img-top"
-                width={"200px"}
-                height={"300px"}
-              />
-              <div className="card-body">
-                <h5 className="card-title">Titulo Pelicula</h5>
-                <div className="row gy-2" id="rowButton">
-                  <a href="#!" className="btn btn-primary">
-                    <i className="bi bi-arrows-angle-expand"></i>
-                    Ver Mas
-                  </a>
-                  <a href="#!" className="btn btn-success">
-                    <i className="bi bi-stopwatch-fill"></i>
-                    Watchlist
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </main>
+      </>
+    );
+  }
 }
