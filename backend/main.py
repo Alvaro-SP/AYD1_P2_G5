14d60ea13d2ab34f72d1150c4be3bad1353ab120
Javier Chávez <3016980070101@ingenieria.usac.edu.gt>
2023-03-15 16:09:34 -0600

    Update Fast-Foward

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
-# from src.Vercatalogo import vercatalogo
-# from src.Cargamasiva import cargamasiva
+from src.Vercatalogo import vercatalogo
+from src.Cargamasiva import cargamasiva
 from src.AddUser import add_user
 from src.LogUser import log_user
 from src.AddWatchlist import addwatchlist
@@ -15,19 +15,19 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
-# #! Endpoint para obtener todas las MOVIES
-# @app.route('/vercatalogo', methods=['GET'])
-# def ver_catalogo():
-#     response = vercatalogo()
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+#! Endpoint para obtener todas las MOVIES
+@app.route('/vercatalogo', methods=['GET'])
+def ver_catalogo():
+    response = vercatalogo()
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-# #! Endpoint para la carga masiva
-# @app.route('/cargamasiva', methods=['POST'])
-# def carga_masiva():
-#     resprev = cargamasiva(request)
-#     resprev.headers.add('Access-Control-Allow-Origin', '*')
-#     return resprev
+#! Endpoint para la carga masiva
+@app.route('/cargamasiva', methods=['POST'])
+def carga_masiva():
+    resprev = cargamasiva(request)
+    resprev.headers.add('Access-Control-Allow-Origin', '*')
+    return resprev
 
 #! Endpoint para agregar un usuario
 @app.route('/register', methods=['POST'])
@@ -45,40 +45,40 @@ def login():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-# #! Endpoint para agregar a la lista WATCHLIST
-# @app.route('/addwatchlist', methods=['POST'])
-# def add_watchlist():
-#     response = addwatchlist(request)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+#! Endpoint para agregar a la lista WATCHLIST
+@app.route('/addwatchlist', methods=['POST'])
+def add_watchlist():
+    response = addwatchlist(request)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-# #! Endpoint para ver la lista de WATCHLIST de un usuario dado su ID
-# @app.route('/verwatchlist', methods=['POST'])
-# def ver_watchlist():
-#     response = verwatchlist(request)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+#! Endpoint para ver la lista de WATCHLIST de un usuario dado su ID
+@app.route('/verwatchlist', methods=['POST'])
+def ver_watchlist():
+    response = verwatchlist(request)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-# #! Endpoint para ver
-# @app.route('/verinfopelicula', methods=['POST'])
-# def ver_peliporuser():
-#     response = getpeliporuser(request)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+#! Endpoint para ver
+@app.route('/verinfopelicula', methods=['POST'])
+def ver_peliporuser():
+    response = getpeliporuser(request)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-# #! Endpoint para promedio
-# @app.route('/promediototal', methods=['POST'])
-# def promedio_total():
-#     response = promediototal(request)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+#! Endpoint para promedio
+@app.route('/promediototal', methods=['POST'])
+def promedio_total():
+    response = promediototal(request)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-# #! Endpoint para quitar watchlist
-# @app.route('/quitwatchlist', methods=['POST'])
-# def quit_watchlist():
-#     response = quitwatchlist(request)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+#! Endpoint para quitar watchlist
+@app.route('/quitwatchlist', methods=['POST'])
+def quit_watchlist():
+    response = quitwatchlist(request)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 #! Endpoint para quitar watchlist
 @app.route('/info-actor', methods=['POST'])
