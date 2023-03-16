4d3d715e06f47a6b16bbcf718d348d407bdea281
Javier Chávez <3016980070101@ingenieria.usac.edu.gt>
2023-03-15 23:31:31 -0600

    Se agregó endpoint de tipo POST para calificar una película.
    - La ruta del endpoint es: /rate-movie
    - Recibe un JSON con el id del usuario que hace la calificación, el id de la película y la calificación respectiva
    - Inserta la calificación y su relación con el usuario y película en la db
    - Si no hay éxito devuelve una respuesta indicando que falló
    - Si hay éxito devuelve la respuesta indicándolo

---
+++ backend/src/RateMovie.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+def rate_movie(request):
+    # Parsear data
+    data = request.get_json()
+
+    # Capturar datos
+    id_user = data['id_user']
+    id_movie = data['id_movie']
+    rate = data['rate']
+
+    # Intentar conexión con la base de datos
+    try:
+        conection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="secret",
+            database="dbayd"
+        )
+        print("Conexión establecida correctamente.")
+
+    except mysql.connector.Error as error:
+        # Retornar error en caso de no poder conectarse
+        print(f"No se pudo conectar a la base de datos: {error}")
+        return {
+            "res": False,
+        }
+
+    # Crear un cursor para interactuar con la base de datos
+    cursor = conection.cursor()
+
+    # Preparar la transacción
+    sql = "INSERT INTO movie_rating (rating, user_iduser, movie_idmovie) VALUES (%s, %s, %s)"
+    vals = (rate, id_user, id_movie)
+
+    # Ejecutar la transacción
+    cursor.execute(sql, vals)
+
+    # Confirmar la inserción en la base de datos
+    conection.commit()
+    print("Calificación insertada correctamente.")
+
+    # Cerrar la conexión
+    conection.close()
+
+    # Retornar respuesta
+    return {
+        "res": True
+    }
